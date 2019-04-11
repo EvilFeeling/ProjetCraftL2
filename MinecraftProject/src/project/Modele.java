5bdeabee1fe0d59adb0e74052ee72e5c7926eb5b
user <user@DESKTOP-K1DJJHL>
2019-04-11 21:12:02 +0200

    Ajout de la base de donné objet : vide,vide,vide;vide,vide,vide;vide,vide,vide

--- MinecraftProject/src/project/Modele.java
+++ MinecraftProject/src/project/Modele.java
@@ -132,3 +132,89 @@ public class Modele extends Application {
         primaryStage.show();
     }
 }
+
+
+
+
+
+//rajout base de données:
+
+
+objet : vide,vide,vide;vide,vide,vide;vide,vide,vide
+
+//objet primaire:
+
+wood
+coal
+flint
+gold
+iron
+leather
+redstone
+rock
+feather
+sugarcane
+string***
+
+
+//objet secondaire:
+
+wood_plank : vide,vide,vide;vide,wood,vide;vide,vide,vide
+stick : vide ,vide ,vide;vide ,wood_plank,vide;vide,wood_plank,vide
+wools : vide,vide,vide;string,string,vide;string,string,vide
+aok_fence_gate : vide,vide,vide;stick,wood_plank,stick;stick,wood_plank,stick
+aok_fence : vide,vide,vide;wood_plank,stick,wood_plank;wood_plank,stick,wood_plank 
+arrow : vide,flint,vide;vide,stick,vide;vide,feather,vide
+boat : vide,vide,vide;wood_plank,vide,wood_plank;wood_plank,wood_plank,wood_plank
+paper: vide,vide,vide;sugarcane,sugarcane,sugarcane;vide,vide,vide
+book : vide,vide,vide;paper,paper,vide;paper,leather,vide
+bookshelf : wood_plank,wood_plank,wood_plank;book,book,book;wood_plank,wood_plank,wood_plank
+bow : vide,stick,string;stick,vide,string;vide,stick,string
+bucket : vide,vide,vide;iron,vide,iron;vide,iron,vide
+chest : wood_plank,wood_plank,wood_plank;wood_plank,vide,wood_plank;wood_plank,wood_plank,wood_plank
+cobblestone_slab : vide,vide,vide;vide,vide,vide;rock,rock,rock
+cobblestone_wall : vide,vide,vide;rock,rock,rock;rock,rock,rock
+compass : vide,iron,vide;iron,redstone,iron;vide,iron,vide
+crafting_table : vide,vide,vide;vide,wood_plank,wood_plank;vide,wood_plank,wood_plank
+dispenser : rock,rock,rock;rock,bow,rock;rock,redstone,rock
+furnace : rock,rock,rock;rock,vide,rock;rock,rock,rock
+lever : vide,vide,vide;vide,stick,vide;vide,rock,vide
+lighter : vide,vide,vide;iron,flint,vide;vide,vide,vide
+watch : vide,gold,vide;gold,redstone,gold;vide,gold,vide
+redstone_torch : vide,vide,vide;vide,redstone,vide,vide,stick,vide
+shear : vide,vide,vide;vide,iron,vide;iron,vide,vide
+fishing_rod : vide,vide,stick;vide,stick,string;stick,vide,string
+piston : wood_plank,wood_plank,wood_plank;rock,iron,rock;rock,redstone,rock
+sign : wood_plank,wood_plank,wood_plank;wood_plank,wood_plank,wood_plank;vide,stick,vide
+woodendoor : wood_plank,wood_plank,vide;wood_plank,wood_plank,vide;wood_plank,wood_plank,vide
+trapdoor : vide,vide,vide;wood_plank,wood_plank,wood_plank;wood_plank,wood_plank,wood_plank
+pressure_plate : vide,vide,vide;wood_plank,wood_plank,vide;vide,vide,vide
+wood_slab : vide,vide,vide;vide,vide,vide;wood_plank,wood_plank,wood_plank
+cartography_table*** : paper,paper,vide;wood_plank,wood_plank,vide;wood_plank,wood_plank,vide
+paint : stick,stick,stick;stick,wools,stick;stick,stick,stick
+music_box : wood_plank,wood_plank,wood_plank;wood_plank,redstone,wood_plank;wood_plank,wood_plank,wood_plank
+bed : vide,vide,vide;wools,wools,wools;wood_plank,wood_plank,wood_plank
+banner : wools,wools,wools;wools,wools,wools;vide,stick,vide
+sugar : vide,vide,vide;vide,sugarcane,vide;vide,vide,vide
+shield : wood_plank,iron,wood_plank;wood_plank,wood_plank,wood_plank;vide,wood_plank,vide
+map : paper,paper,paper;paper,compass,paper;paper,paper,paper
+
+//armure :
+
+iron_boots : vide,vide,vide;iron,vide,iron;iron,vide,iron
+iron_leggings : iron,iron,iron;iron,vide,iron;iron,vide,iron
+ironchesplate : iron,vide,iron;iron,iron,iron;iron,iron,iron
+ironhelmet : iron,iron,iron;iron,vide,iron;vide,vide,vide
+
+//arme :
+
+stone_axe : rock,rock,vide;rock,stick,vide;vide,stick,vide
+stone_pickaxe : rock,rock,rock;vide,stick,vide;vide,stick,vide
+stone_shovel : vide,rock,vide;vide,stick,vide;vide,stick,vide
+stone_sword : vide,rock,vide;vide,rock,vide;vide,stick,vide
+
+wooden_axe : wood_plank,wood_plank,vide;wood_plank,stick,vide;vide,stick,vide
+wooden_pickaxe : wood_plank,wood_plank,wood_plank;vide,stick,vide;vide,stick,vide
+wooden_shovel : vide,wood_plank,vide;vide,stick,vide;vide,stick,vide
+wooden_sword*** : vide,wood_plank,vide;vide,wood_plank,vide;vide,stick,vide
+
